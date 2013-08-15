ba0c987cf10da86d44600c71fdb723663c15b560
Joe Fitzgerald <joeyfitz@gmail.com>
2013-08-15 15:20:55 -0600

    Updating license and metadata

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name             'chocolatey-installer'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures chocolatey-installer'
+maintainer       'Joe Fitzgerald'
+maintainer_email 'joe.fitzgerald@emc.com'
+license          'Apache 2.0'
+description      'Installs packages using Chocolatey'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends          'windows'
